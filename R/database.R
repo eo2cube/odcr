ecf790510cc9019ab9f8b23e32eadb29486dcbff
16EAGLE <jxsw@web.de>
2021-07-10 13:06:25 +0200

    add examples

--- R/database.R
+++ R/database.R
@@ -8,6 +8,21 @@
 #' @return
 #' None or the connection of class 'datacube.api.core.Datacube', if \code{object = T}
 #'
+#' @examples
+#' \dontrun{
+#' library(odcr)
+#'
+#' # connect to a database, store the Daatcube connection internally (default and recommended)
+#' database_connect(app = "Sentinel_2")
+#'
+#' # retrieve the current connection as an object
+#' dc <- database_get_active()
+#'
+#' # or return a Datacube object
+#' dc <- database_connect(app = "Sentinel_2", object = TRUE)
+#'
+#'
+#' }
 #' @export
 #' @name database
 database_connect <- function(..., object = F){
